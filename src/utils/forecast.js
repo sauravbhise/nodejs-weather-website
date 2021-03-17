484636ff12b0323314edded23de49fee67fd49ae
Saurav Bhise <sauravbhise2002@gmail.com>
2021-03-17 23:13:14 +0530

    Add feels-like parameter to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,10 +14,17 @@ const forecast = (latitude, longtitude, callback) => {
       callback("Unable to find weather. Try diffrent co-ordinates", undefined);
     } else {
       const temp = body.current.temp;
+      const feelsLike = body.current.feels_like;
       const visibility = body.current.visibility;
       callback(
         undefined,
-        "It is currently " + temp + " out and visibility is " + visibility
+        "It is currently " +
+          temp +
+          " out " +
+          ", it feels like " +
+          feelsLike +
+          "and the visibility is " +
+          visibility
       );
     }
   });
